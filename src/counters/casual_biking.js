019cefe1b1663ed112bc175cabbc9e6753a9b0c1
Ivan Eisenberg <ivaneisenberg@northwesternmutual.com>
2017-10-21 11:51:36 -0500

    added mathjs because I have never played with it. Fixed rounding error on casual cycling

--- src/counters/casual_biking.js
+++ src/counters/casual_biking.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import math from 'mathjs'
 
 export default (calories, { resultText, resultHighlightText }) => {
   const cyclingCaloriesPerHour = 396
   const cyclingMinutes = Math.ceil(parseInt(calories, 10) / (cyclingCaloriesPerHour / 60))
-  const cyclingMiles = (cyclingMinutes / 60) * 12
+  const cyclingMiles = Math.round(math.eval((cyclingMinutes / 60) * 12))
   return <p className={ resultText }>Biking causally, say 12mph or so, will burn about 396 calories in an hour. So if you <span className={ resultHighlightText }>bike for an extra { cyclingMinutes } minutes, or about { cyclingMiles } miles</span> you will burn it off.</p>
 }
 
